ca83437de25eef576004765f70be3914b3098dcf
Alfredo Limin <jjlimin@yahoo.com>
2026-05-09 19:03:46 +0300

    added logs for finisher lambda function

--- backend/lambdas/CodeAnimator-Notifier/lambda_function.py
+++ backend/lambdas/CodeAnimator-Notifier/lambda_function.py
@@ -1,8 +1,14 @@
 import json
 import boto3
 import os
+import logging
 from datetime import datetime
 
+# initialize logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# initialize clients
 s3 = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CodeAnimatorTable')
@@ -10,12 +16,15 @@ table = dynamodb.Table('CodeAnimatorTable')
 BUCKET_NAME = 'code-animator-assets'
 PRESIGNED_URL_EXPIRY = 60 * 60 * 24 * 7  # 7 days
 
-
 def lambda_handler(event, context):
+    request_id = context.aws_request_id
     user_id = event.get('UserID')
     job_id = event.get('ProjectID')
 
+    logger.info(f"[request_id:{request_id}] Starting Finisher Lambda for UserID: {user_id}, ProjectID: {job_id}")
+
     if not user_id or not job_id:
+        logger.warning(f"[request_id:{request_id}] No UserID or ProjectID in event")
         raise ValueError(f"Missing UserID or ProjectID in event: {event}")
 
     video_s3_key = f'projects/{job_id}/animation.mp4'
@@ -28,6 +37,7 @@ def lambda_handler(event, context):
     )
 
     # Mark job as finished in DynamoDB and store the video URL
+    logger.info(f"[request_id:{request_id}] Updating DynamoDB status to 'Done' and storing video URL: {video_url}")
     table.update_item(
         Key={'UserID': user_id, 'ProjectID': f'PROJ#{job_id}'},
         UpdateExpression="set #s = :status_val, VideoS3Key = :s3key, VideoURL = :url, FinishedAt = :ts",
@@ -40,8 +50,7 @@ def lambda_handler(event, context):
         }
     )
 
-    print(f"Job {job_id} finished. Video at s3://{BUCKET_NAME}/{video_s3_key}")
-
+    logger.info(f"[request_id:{request_id}] Job {job_id} finished. Video at s3://{BUCKET_NAME}/{video_s3_key}")
     return {
         'statusCode': 200,
         'UserID': user_id,
